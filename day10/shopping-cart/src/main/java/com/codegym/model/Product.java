b9e8bab7839fc677573ef8319593d34c09cdf335
cattuong28597 <cattuong28597@gmail.com>
2021-12-04 09:46:48 +0700

    lan1

---
+++ day10/shopping-cart/src/main/java/com/codegym/model/Product.java
@@ -0,0 +1,23 @@
+package com.codegym.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+    private String description;
+
+}
