d36d1c3acc1c693029284af17d91997ea07a7dbd
cattuong28597 <cattuong28597@gmail.com>
2021-12-06 11:26:32 +0700

    lan1

---
+++ day11/customer-manage-RESTful/src/main/java/com/codegym/cms/model/Customer.java
@@ -0,0 +1,26 @@
+package com.codegym.cms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
