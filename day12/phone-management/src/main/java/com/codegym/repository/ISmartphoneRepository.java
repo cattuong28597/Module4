bdb61c94725fc5db2cf2087ce80e66c9623981e9
cattuong28597 <cattuong28597@gmail.com>
2021-12-07 15:36:27 +0700

    lan1

---
+++ day12/phone-management/src/main/java/com/codegym/repository/ISmartphoneRepository.java
@@ -0,0 +1,8 @@
+package com.codegym.repository;
+
+import com.codegym.model.Smartphone;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+public interface ISmartphoneRepository extends CrudRepository<Smartphone, Long> {
+}
