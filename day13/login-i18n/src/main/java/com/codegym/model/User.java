9031d3ca7610e5453b08a94102bf16ade8906666
cattuong28597 <cattuong28597@gmail.com>
2021-12-08 10:44:49 +0700

    lan1

---
+++ day13/login-i18n/src/main/java/com/codegym/model/User.java
@@ -0,0 +1,16 @@
+package com.codegym.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+
+    private Long id;
+    private String username;
+    private String password;
+
+}
